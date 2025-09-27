38dc4c36aac24ddb35cc30ed961a33e6b4ed6d5c
rhit-manchas <manchas@rose-hulman.edu>
2025-09-27 17:00:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
+// 1. Create the buttons in the javascript to begin with and load them in each HTML file
 
+// 2. Create a second interactive part (using JS) - Interative buttions on my portfolio
+
+/*
+<!--Nav Bar
+    <ul>
+      <li><a href="#Main Page">Main Page</a></li>
+      <li><a href="#Portfolio">Portfolio</a></li>
+      <li><a href="#Resume">Resume</a></li>
+    </ul>-->
+
+<div class= "footer">
+      <p>Connect With Me:</p>
+      <p>
+        <a href="">LinkedIn</a>
+        <a href="">Handshake</a>
+      </p>
+    </div>
+*/
